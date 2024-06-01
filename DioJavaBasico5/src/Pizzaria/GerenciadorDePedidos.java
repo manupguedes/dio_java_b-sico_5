98f0cbec00113ad7475e293b7f558217c15251b5
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 17:19:02 -0300

    GerenciadorDePedidos.java

--- DioJavaBasico5/src/Pizzaria/GerenciadorDePedidos.java
+++ DioJavaBasico5/src/Pizzaria/GerenciadorDePedidos.java
@@ -1,5 +1,33 @@
 package Pizzaria;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GerenciadorDePedidos {
+    private static GerenciadorDePedidos instancia;
+    private List<Pizza> pedidos;
 
+    private GerenciadorDePedidos() {
+        pedidos = new ArrayList<>();
+    }
+
+    public static GerenciadorDePedidos getInstance() {
+        if (instancia == null) {
+            instancia = new GerenciadorDePedidos();
+        }
+        return instancia;
+    }
+
+    public void adicionarPedido(Pizza pizza) {
+        pedidos.add(pizza);
+    }
+
+    public List<Pizza> getPedidos() {
+        return pedidos;
+    }
+
+    public void mostrarPedidos() {
+        for (Pizza pizza : pedidos) {
+            System.out.println(pizza);
+        }
+    }
 }
