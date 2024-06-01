14581affb52f596f885815bb431d24ea45d39778
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 17:19:53 -0300

    Pizza.java

--- DioJavaBasico5/src/Pizzaria/Pizza.java
+++ DioJavaBasico5/src/Pizzaria/Pizza.java
@@ -1,5 +1,50 @@
 package Pizzaria;
-
 public class Pizza {
+    private String massa;
+    private String molho;
+    private String recheio;
+    private boolean queijoExtra;
 
+    private Pizza(Builder builder) {
+        this.massa = builder.massa;
+        this.molho = builder.molho;
+        this.recheio = builder.recheio;
+        this.queijoExtra = builder.queijoExtra;
+    }
+
+    public static class Builder {
+        private String massa;
+        private String molho;
+        private String recheio;
+        private boolean queijoExtra;
+
+        public Builder massa(String massa) {
+            this.massa = massa;
+            return this;
+        }
+
+        public Builder molho(String molho) {
+            this.molho = molho;
+            return this;
+        }
+
+        public Builder recheio(String recheio) {
+            this.recheio = recheio;
+            return this;
+        }
+
+        public Builder queijoExtra(boolean queijoExtra) {
+            this.queijoExtra = queijoExtra;
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza [massa=" + massa + ", molho=" + molho + ", recheio=" + recheio + ", queijoExtra=" + queijoExtra + "]";
+    }
 }
