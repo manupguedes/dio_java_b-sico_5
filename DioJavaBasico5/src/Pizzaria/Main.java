629e2ae4ae7b4b6c76bf371f30baec2b7bddeafa
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 17:19:25 -0300

    Main.java

--- DioJavaBasico5/src/Pizzaria/Main.java
+++ DioJavaBasico5/src/Pizzaria/Main.java
@@ -1,5 +1,24 @@
 package Pizzaria;
-
 public class Main {
+    public static void main(String[] args) {
+        Pizza pizza1 = new Pizza.Builder()
+                .massa("Tradicional")
+                .molho("Tomate")
+                .recheio("Calabresa")
+                .queijoExtra(true)
+                .build();
 
+        Pizza pizza2 = new Pizza.Builder()
+                .massa("Fina")
+                .molho("Branco")
+                .recheio("Frango")
+                .queijoExtra(false)
+                .build();
+
+        GerenciadorDePedidos gerenciador = GerenciadorDePedidos.getInstance();
+        gerenciador.adicionarPedido(pizza1);
+        gerenciador.adicionarPedido(pizza2);
+
+        gerenciador.mostrarPedidos();
+    }
 }
